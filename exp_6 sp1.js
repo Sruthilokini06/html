09c986df15f2f938ae770282b9272bbefef679ac
Sruthi <lokinisruthi994@gmail.com>
2026-02-18 11:40:10 +0530

    Add files via upload

---
+++ exp_6 sp1.js
@@ -0,0 +1,12 @@
+
+const person = {
+  name: "John",
+  age: 25,
+  city: "New York"
+};
+
+const { name, age, city } = person;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("City:", city);
